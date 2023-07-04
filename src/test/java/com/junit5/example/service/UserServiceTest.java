47f56dd1695280559ad7f9c8f843843f185c6da6
DeevS <deev@advantum.pro>
2023-07-04 18:05:30 +0300

    Start project. Create first unit test

---
+++ src/test/java/com/junit5/example/service/UserServiceTest.java
@@ -0,0 +1,15 @@
+package com.junit5.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class UserServiceTest {
+
+    @Test
+    void test() {
+
+        assertTrue(1 > 2);
+    }
+
+}
