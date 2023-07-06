26427aa77376a631cf23478ab9f3aa834fc9ce60
DeevS <deev@advantum.pro>
2023-07-06 10:00:33 +0300

    Mockito spy

--- src/test/java/com/junit5/example/service/UserServiceWithMocksTest.java
+++ src/test/java/com/junit5/example/service/UserServiceWithMocksTest.java
@@ -2,11 +2,14 @@ package com.junit5.example.service;
 
 import com.junit5.example.dao.UserDao;
 import com.junit5.example.dto.User;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class UserServiceWithMocksTest {
 
@@ -14,7 +17,7 @@ public class UserServiceWithMocksTest {
     public static final User PETR = User.of(2, "Petr", "111");
 
     @Test
-    void delete() {
+    void deleteWithMock() {
         UserDao userDao = mock(UserDao.class);
         doReturn(true).when(userDao).delete(IVAN.getId());
 //        when(userDao.delete(IVAN.getId())).thenReturn(true);
@@ -24,6 +27,27 @@ public class UserServiceWithMocksTest {
 
         boolean result = userService.delete(IVAN.getId());
 
-        Assertions.assertThat(result).isTrue();
+        assertThat(result).isTrue();
     }
+
+    @Test
+    void deleteWithSpy() {
+        UserDao userDao = spy(new UserDao());
+        doReturn(true).when(userDao).delete(IVAN.getId());
+//        when(userDao.delete(IVAN.getId())).thenReturn(true);
+        UserService userService = new UserService(userDao);
+
+        userService.add(IVAN);
+
+        boolean result = userService.delete(IVAN.getId());
+
+        assertThat(result).isTrue();
+
+        userService.delete(IVAN.getId());
+        userService.delete(IVAN.getId());
+
+        verify(userDao, Mockito.times(3)).delete(IVAN.getId());
+
+    }
+
 }
